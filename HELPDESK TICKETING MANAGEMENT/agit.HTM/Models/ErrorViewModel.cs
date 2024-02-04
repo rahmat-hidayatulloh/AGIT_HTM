d2c47e8454768c320066b6918ec97101697384be
rohid <rahmat.hidayatulloh@atlantica.dev>
2024-02-04 11:29:50 +0700

    msg: master commit

---
+++ HELPDESK TICKETING MANAGEMENT/agit.HTM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace agit.HTM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
